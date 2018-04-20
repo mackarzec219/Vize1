a3e8309714bf8ffb1c16c2362fbaf9034c1f7784
mackarzec219 <38542295+mackarzec219@users.noreply.github.com>
2018-04-20 23:00:58 +0300

    Update Form1.cs

--- S1/Form1.cs
+++ S1/Form1.cs
@@ -16,43 +16,43 @@ namespace $safeprojectname$
             InitializeComponent();
         }
 
-        int hak = 0, sayi_tahmin, randomsayi; // Değişkenler tanımlandı
-        private void Form1_Load(object sender, EventArgs e) // Random sayı aldırma
+        int hak = 0, sayi_tahmin, randomsayi; 
+        private void Form1_Load(object sender, EventArgs e) 
         {
-            Random rnd = new Random(); // Random sayi bir değişkene atandı.
-            randomsayi = rnd.Next(0, 100); // 0-100 arasında alınan random değer sayi değişkenine aktarıldı
+            Random rnd = new Random(); 
+            randomsayi = rnd.Next(0, 100); 
 
         }
-        private void button1_Click(object sender, EventArgs e) // Butona tıklandıktan sonra olucak olan işlemler
+        private void button1_Click(object sender, EventArgs e) 
         {
-            if (textBox1.Text != "") // textBox1 ın boş mu bolumu onu kontrol ediyoruz.
+            if (textBox1.Text != "") 
             {
-                if (hak < 10) // Eğer hak 10'dan küçükse (0-10 arasında 10 dahil değil)
+                if (hak < 10) 
                 {
-                    sayi_tahmin = int.Parse(textBox1.Text); // textBox1'e girilen sayıyı tahmini_Sayi değişkenine atadık
-                    if (sayi_tahmin < randomsayi) // Eğer tahmin sayımız random sayıdan küçükse
+                    sayi_tahmin = int.Parse(textBox1.Text); 
+                    if (sayi_tahmin < randomsayi) 
                     {
-                        listBox1.Items.Add(sayi_tahmin); // Tahmini sayıyı listBox1'e ekle
-                        textBox1.Text = ""; // textBox1'in içini temizle
-                        hak++; // Hakkı 1 arttır
-                        label2.Text = "Girdiğiniz sayı tahmini sayıdan küçük."; // label2'ye uyarı yazdır
+                        listBox1.Items.Add(sayi_tahmin); 
+                        textBox1.Text = ""; 
+                        hak++; 
+                        label2.Text = "Girdiğiniz sayı tahmini sayıdan küçük.";
                     }
-                    else if (sayi_tahmin > randomsayi) // Eğer tahmin sayımız random sayıdan büyükse
+                    else if (sayi_tahmin > randomsayi)
                     {
-                        listBox1.Items.Add(sayi_tahmin); // Tahmini sayıyı listBox1'e ekle
-                        textBox1.Text = ""; // textBox1'in içini temizle
-                        hak++; // Hakkı 1 arttır
-                        label2.Text = "Girdiğiniz sayı tahmini sayıdan büyük."; // label2'ye uyarı yazdır
+                        listBox1.Items.Add(sayi_tahmin); 
+                        textBox1.Text = ""; 
+                        hak++; 
+                        label2.Text = "Girdiğiniz sayı tahmini sayıdan büyük."; 
                     }
-                    else if (sayi_tahmin == randomsayi) // Eğer tahmin sayımız random sayıya eşitse
-                        MessageBox.Show("Helal be Koçum! Doğru sayıyı bularak oyunu başarıyla tamamladınız.", "Tebrikler!"); // Tebrik mesajı göster
+                    else if (sayi_tahmin == randomsayi) 
+                        MessageBox.Show("Helal be Koçum! Doğru sayıyı bularak oyunu başarıyla tamamladınız.", "Tebrikler!"); 
                 }
                 else
                     MessageBox.Show("Kaybettiniz!", "Maalesef 10 hakkınızın tamamını kullandınız.");
             }
-            else if (textBox1.Text == "") // textBox1'in dolu olup olmadığını kontrol ediyoruz
+            else if (textBox1.Text == "") 
             {
-                MessageBox.Show("Lütfen tahmin sayınızı giriniz."); // textBox1 boş ise uyarı mesajı ver
+                MessageBox.Show("Lütfen tahmin sayınızı giriniz."); 
             }
         }
     }
