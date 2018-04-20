8662951a2840532b0470dd869f9e1e2c5eafc864
mackarzec219 <38542295+mackarzec219@users.noreply.github.com>
2018-04-20 23:09:35 +0300

    Update Form1.cs

--- S1/Form1.cs
+++ S1/Form1.cs
@@ -8,52 +8,51 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace $safeprojectname$
-{
+            {
     public partial class Form1 : Form
-    {
+            {
         public Form1()
-        {
+            {
             InitializeComponent();
-        }
+            }
 
         int hak = 0, tahmin_sayi, randomsayi; 
         private void Form1_Load(object sender, EventArgs e) 
-        {
+            {
             Random rnd = new Random(); 
             randomsayi = rnd.Next(0, 100); 
-
-        }
+            }
         private void button1_Click(object sender, EventArgs e) 
-        {
+            {
             if (textBox1.Text != "") 
             {
                 if (hak < 10) 
-                {
+            {
                     tahmin_sayi = int.Parse(textBox1.Text); 
                     if (tahmin_sayi < randomsayi) 
-                    {
+            {
                         listBox1.Items.Add(tahmin_sayi); 
                         textBox1.Text = ""; 
                         hak++; 
-                        label2.Text = "Girdiğiniz sayı tahmini sayıdan küçük.";
-                    }
+                        label2.Text = "Girilen sayı tahmin edilen sayıdan küçük.";
+            }
                     else if (tahmin_sayi > randomsayi)
-                    {
+            {
                         listBox1.Items.Add(tahmin_sayi); 
                         textBox1.Text = ""; 
                         hak++; 
-                        label2.Text = "Girdiğiniz sayı tahmin sayıdan büyük."; 
-                    }
+                        label2.Text = "Girilen sayı tahmin edilen sayıdan büyük."; 
+            }
                     else if (tahmin_sayi == randomsayi) 
-                        MessageBox.Show("Doğru sayıyı buldunuz.", "Tebrikler!"); 
-                }
+                        MessageBox.Show("Doğru sayıyı buldunuz.", "Tebrikler."); 
+            }
                 else
-                    MessageBox.Show("Hakkınız bitti", "Maalesef 10 hakkınızın tamamını kullandınız.");
+                    MessageBox.Show("Hakkınız bitti", "Maalesef haklarınızın hepsini kullandınız.");
             }
             else if (textBox1.Text == "") 
             {
                 MessageBox.Show("Lütfen tahmin sayınızı giriniz."); 
             }
-        }
-    }
-}
+            }
+            }
+            }
